cf6e16f4263780fc4a1c5c59f5043e2082e40ea9
anliec <nicolas.six@pau.fr>
2015-05-18 15:35:51 +0200

    comment for javadoc on MainWindows

--- AGameOfLife/src/Game/MainWindows.java
+++ AGameOfLife/src/Game/MainWindows.java
@@ -20,7 +20,6 @@ public class MainWindows extends JFrame {
     protected Button btNextGeneration;
     protected JMenuBar menuBar;
     protected String title;
-
     protected JFileChooser fc;
 
     /**
@@ -104,6 +103,9 @@ public class MainWindows extends JFrame {
         menuBar.add(helpMenu);
     }
 
+    /**
+     * open the file choosing windows and manage its return in order to open files
+     */
     public void openFileMenu(){
         fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
         if(fc.showOpenDialog(this) == JFileChooser.APPROVE_OPTION){
@@ -112,6 +114,9 @@ public class MainWindows extends JFrame {
         }
     }
 
+    /**
+     * open the file choosing windows and manage its return in order to save files
+     */
     public void saveFileMenu(){
         fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
         if(fc.showSaveDialog(this) == JFileChooser.APPROVE_OPTION){
@@ -119,6 +124,9 @@ public class MainWindows extends JFrame {
         }
     }
 
+    /**
+     * manage the initialisation of a new board
+     */
     public void initialisationOfTheBoard(){
         System.out.println("init board");
         //board initialisation:
@@ -131,10 +139,16 @@ public class MainWindows extends JFrame {
         board.setTeams(teams);
 
         //setup of the teams cells:
-        boardIni(1,board);
+        boardIni(1, board);
 
     }
 
+    /**
+     * add the team given to the board if the team is not an IA the
+     * method show an interface for the user to place is cells
+     * @param team number of the team to init
+     * @param board the board where the team will be added
+     */
     private void boardIni(int team, Board board){
         if(team < gameOptions.getTeamsIA().size()){
             if(board.getTeam(team).isIA()){
@@ -176,10 +190,17 @@ public class MainWindows extends JFrame {
 
     }
 
+    /**
+     * end the placement of the currant human team
+     */
     public void endOfHumanPlacement(){
         boardIni(iniBoard.getCurrantTeam()+1,iniBoard.getBoard());
     }
 
+    /**
+     * set the interface in playing mode
+     * @param board the board which will be showed at the first turn
+     */
     public void setPlayMode(Board board){
         System.out.println("play mode");
         try{
