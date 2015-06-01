c97acb93b907f1842b7826e3e6e6840086f242a6
anliec <nicolas.six@pau.fr>
2015-06-01 13:30:14 +0200

    addition of license file

--- AGameOfLife/src/Game/AboutWindow.java
+++ AGameOfLife/src/Game/AboutWindow.java
@@ -1,3 +1,20 @@
+/**This file is part of A Game Of Life.
+
+ Game Of Life is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Game Of Life is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Game Of Life.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+
 package Game;
 
 import javax.imageio.ImageIO;
@@ -19,8 +36,8 @@ public class AboutWindow extends JFrame {
     
     public AboutWindow() {
         setTitle("About us");
-        this.setPreferredSize(new Dimension(500,490));
-        this.setMinimumSize(new Dimension(500, 490));
+        this.setPreferredSize(new Dimension(500,480));
+        this.setMinimumSize(new Dimension(500, 480));
         this.setResizable(false);
         Container pane = this.getContentPane();
         BoxLayout layout = new BoxLayout(pane, BoxLayout.PAGE_AXIS);
